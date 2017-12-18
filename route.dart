2bdfc55509e01eb2483e35eee7056f3f9f27a209
mikes jeff <swipe@mikess-MacBook-Pro.local>
2017-12-18 17:35:49 +0100

    added a lot of bloat

---
+++ route.dart
@@ -0,0 +1,32 @@
+import 'dart:core';
+import 'dart:async';
+import 'dart:io';
+
+
+Stream<int> getthings() async* {
+
+  while(true) {
+    sleep(new Duration(seconds: 1));
+    yield 66;
+  }
+}
+
+Future main() async {
+
+  Stopwatch perf = new Stopwatch();
+  perf.start();
+  Stream m = getthings().asBroadcastStream();
+  perf.stop();
+
+  print("spent ${perf.elapsedMilliseconds} ms");
+
+  m.listen((data) {
+    print("listening to ${data}");
+  });
+
+  m.listen((data) {
+    print("also listening: ${data}");
+  });
+
+
+}
