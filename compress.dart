6e76ce56e563e3532c287c2f0b171ad77ce16c72
swipe_06 <laszloekovacs@gmail.com>
2017-12-23 23:10:28 +0100

    cleanup

--- compress.dart
+++ compress.dart
@@ -5,8 +5,7 @@ void main() {
   var utf8_codec = new Utf8Codec();
   var gzip_codec = new GZipCodec();
 
-  List<int> data =
-      gzip_codec.encode(utf8_codec.encode("hello from dart! compress this"));
+  List<int> data = gzip_codec.encode(utf8_codec.encode("hello from dart! compress this"));
 
   for (var code in data) {
     print(code.toString());
